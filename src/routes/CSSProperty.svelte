0640a19c96339083978f77552351e9429e14bf88
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-20 11:19:53 +0530

    chore: update CSSProperty component

--- src/routes/CSSProperty.svelte
+++ src/routes/CSSProperty.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
-	import { Options } from './box';
-	export let property: string;
-	export let selectedOption: string | undefined;
+	export let title: string;
+	export let options: string[];
+	export let selected: string = options[0];
 </script>
 
-<label class="flex flex-col w-min p-1">
-	<p class="text-xs sm:text-sm">{property}</p>
-	<select bind:value={selectedOption} class="inline-block max-w-fit max p-2 mb-1 text-xs sm:text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-		{#each Options[property].options as option}
-			<option value={option}>
-				{option}
-			</option>
-		{/each}
-	</select>
-</label>
+<select bind:value={selected} class="inline-block max p-2 text-xs sm:text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+	{#each options as option}
+		<option value={option}>
+			{title + ': ' + option}
+		</option>
+	{/each}
+</select>
+
+<!-- <label class="flex flex-col w-min">
+	 <p class="text-xs sm:text-sm">{title}</p> 
+</label> -->
+
+<style lang="postcss">
+	select {
+		@apply transition-colors duration-200 ease-in-out;
+	}
+</style>
