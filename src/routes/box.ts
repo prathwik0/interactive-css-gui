a47cec8afa23eae2d55304ed9f3ddef5574a56e8
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-20 11:19:06 +0530

    feat: create TS classes, update options
    - Remove BoxInterface and other Interfaces
    - Create BoxProperties, BlockProperties and FlexProperties classes instead
    - Change StackInterface to class Stack
    - Update options
    - Create type Options for options (to provide TS benefits)

--- src/routes/box.ts
+++ src/routes/box.ts
@@ -1,169 +1,336 @@
-export interface BoxInterface {
-	display: string;
-	width: {
-		value: number;
-		unit: string;
-	};
-	height: {
-		value: number;
-		unit: string;
-	};
-	padding: {
-		value: number;
-		unit: string;
-	};
-	border: {
-		value: number;
-		unit: string;
-	};
-	margin: {
-		value: number;
-		unit: string;
+/**
+ * Type Unit = 'px' | 'rem' | 'em' | '%' | 'vw' | 'vh'
+ */
+type Unit = 'px' | 'rem' | 'em' | '%' | 'vw' | 'vh';
+
+/**
+ * BoxModelPropertyValues is a class that represents the values of a box model property such as width, padding, border etc.
+ */
+class BoxModelPropertyValues {
+	selected: 'numeric' | 'string' = 'numeric';
+	string: 'auto' | 'max-content' | 'min-content' = 'auto';
+	numeric: { num: number; unit: Unit } = {
+		num: 0,
+		unit: 'px',
 	};
 }
 
-export interface FlexInterface extends BoxInterface {
-	'flex-direction'?: string;
-	'flex-wrap'?: string;
-	'justify-content'?: string;
-	'align-items'?: string;
-	'align-content'?: string;
+/**
+ * BoxProperties is a class with display = block and box model properties
+ */
+class BoxProperties {
+	boxModel: {
+		width: BoxModelPropertyValues;
+		height: BoxModelPropertyValues;
+		padding: BoxModelPropertyValues;
+		border: BoxModelPropertyValues;
+		margin: BoxModelPropertyValues;
+	} = {
+		width: new BoxModelPropertyValues(),
+		height: new BoxModelPropertyValues(),
+		padding: new BoxModelPropertyValues(),
+		border: new BoxModelPropertyValues(),
+		margin: new BoxModelPropertyValues(),
+	};
+
+	constructor() {
+		this.boxModel.width.selected = 'numeric';
+		this.boxModel.width.numeric.num = 15;
+		this.boxModel.width.numeric.unit = 'rem';
+
+		this.boxModel.height.selected = 'numeric';
+		this.boxModel.height.numeric.num = 15;
+		this.boxModel.height.numeric.unit = 'rem';
+
+		this.boxModel.border.selected = 'numeric';
+		this.boxModel.border.numeric.num = 0.25;
+		this.boxModel.border.numeric.unit = 'rem';
+	}
 }
 
-export interface FlexStackInterface {
-	currentBox: FlexInterface;
-	count: number;
-	children: FlexStackInterface[];
+export class BlockProperties extends BoxProperties {
+	display: 'block' = 'block';
+}
+
+/**
+ * FlexProperties is a class with display = flex, box model properties and flex properties
+ */
+export class FlexProperties extends BoxProperties {
+	display: 'flex' = 'flex';
+	flex: {
+		'flex-direction': 'row' | 'row-reverse' | 'column' | 'column-reverse';
+		'flex-wrap': 'nowrap' | 'wrap' | 'wrap-reverse';
+		'justify-content'?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
+		'align-items'?: 'flex-start' | 'flex-end' | 'center' | 'stretch';
+		'align-content'?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
+	} = {
+		'flex-direction': 'row',
+		'flex-wrap': 'nowrap',
+	};
+}
+
+/**
+ * Stack is a class that represents a stack of boxes
+ */
+export class Stack {
+	currentBox: BlockProperties | FlexProperties = new FlexProperties();
+	count: number = 0;
+	children: Stack[] = [];
 }
 
 /* ---------------------------------------------------------------------- */
 /* ----------------------------- EXAMPLES ------------------------------- */
 /* ---------------------------------------------------------------------- */
 
-export const smallBox: BoxInterface = {
-	display: 'block',
-	width: {
-		value: 8,
-		unit: 'rem',
-	},
-	height: {
-		value: 8,
-		unit: 'rem',
-	},
-	padding: {
-		value: 2,
-		unit: 'rem',
-	},
-	border: {
-		value: 0.25,
-		unit: 'rem',
-	},
-	margin: {
-		value: 0,
-		unit: 'rem',
-	},
-};
-
-export const initialFlexBox = {
-	display: 'flex',
-	'flex-direction': 'row',
-	width: {
-		value: 8,
-		unit: 'rem',
-	},
-	height: {
-		value: 8,
-		unit: 'rem',
-	},
-	padding: {
-		value: 2,
-		unit: 'rem',
-	},
-	border: {
-		value: 0.25,
-		unit: 'rem',
-	},
-	margin: {
-		value: 0,
-		unit: 'rem',
-	},
-};
-
-export const smallStack: FlexStackInterface = {
-	currentBox: structuredClone(initialFlexBox),
-	count: 0,
-	children: [],
-};
-
 /* ---------------------------------------------------------------------- */
 /* ----------------------------- OPTIONS! ------------------------------- */
 /* ---------------------------------------------------------------------- */
 
-export const Options: any = {
-	display: {
-		options: ['block', 'inline', 'inline-block', 'flex', 'none'],
-	},
-	'flex-direction': {
-		options: ['row', 'row-reverse', 'column', 'column-reverse'],
-	},
-	'flex-wrap': {
-		options: ['wrap', 'nowrap', 'wrap-reverse'],
-	},
-	'justify-content': {
-		options: ['flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'space-evenly'],
-	},
-	'align-items': {
-		options: ['flex-start', 'flex-end', 'center', 'stretch', 'baseline'],
-	},
-	'align-content': {
-		options: ['flex-start', 'flex-end', 'center', 'stretch', 'space-between', 'space-around', 'space-evenly'],
-	},
-	unit: {
-		options: ['vw', 'vh', '%', 'px', 'em', 'rem'],
-		vw: {
-			range: [0, 100],
-			step: 2,
-		},
-		vh: {
-			range: [0, 100],
-			step: 2,
-		},
-		'%': {
-			range: [0, 100],
-			step: 2,
-		},
+type OptionsFlex = 'flex-direction' | 'flex-wrap' | 'justify-content' | 'align-items' | 'align-content';
+
+interface OptionsBoxModelProperty {
+	options: string[];
+	string: string[];
+	numeric: {
+		options: string[];
 		px: {
-			range: [0, 1200],
-			step: 4,
-		},
-		em: {
-			range: [0, 80],
-			step: 1,
-		},
+			range: [number, number];
+			step: number;
+		};
 		rem: {
-			range: [0, 80],
-			step: 1,
+			range: [number, number];
+			step: number;
+		};
+		em: {
+			range: [number, number];
+			step: number;
+		};
+		'%': {
+			range: [number, number];
+			step: number;
+		};
+		vw: {
+			range: [number, number];
+			step: number;
+		};
+		vh: {
+			range: [number, number];
+			step: number;
+		};
+	};
+}
+
+type Options = {
+	display: {
+		options: string[];
+	};
+	flex: {
+		options: OptionsFlex[];
+		'flex-direction': {
+			options: ('row' | 'row-reverse' | 'column' | 'column-reverse')[];
+		};
+		'flex-wrap': {
+			options: ('wrap' | 'nowrap' | 'wrap-reverse')[];
+		};
+		'justify-content': {
+			options: ('initial' | 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly')[];
+		};
+		'align-items': {
+			options: ('flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline')[];
+		};
+		'align-content': {
+			options: ('flex-start' | 'flex-end' | 'center' | 'stretch' | 'space-between' | 'space-around' | 'space-evenly')[];
+		};
+	};
+	boxModel: {
+		options: string[];
+		width: OptionsBoxModelProperty;
+		height: OptionsBoxModelProperty;
+		padding: OptionsBoxModelProperty;
+		border: OptionsBoxModelProperty;
+		margin: OptionsBoxModelProperty;
+	};
+};
+
+export const options: Options = {
+	display: {
+		options: ['block', 'inline', 'inline-block', 'grid', 'flex', 'contents', 'none'],
+	},
+	flex: {
+		options: ['flex-direction', 'flex-wrap', 'justify-content', 'align-items', 'align-content'],
+		'flex-direction': {
+			options: ['row', 'row-reverse', 'column', 'column-reverse'],
+		},
+		'flex-wrap': {
+			options: ['wrap', 'nowrap', 'wrap-reverse'],
+		},
+		'justify-content': {
+			options: ['initial', 'flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'space-evenly'],
+		},
+		'align-items': {
+			options: ['flex-start', 'flex-end', 'center', 'stretch', 'baseline'],
+		},
+		'align-content': {
+			options: ['flex-start', 'flex-end', 'center', 'stretch', 'space-between', 'space-around', 'space-evenly'],
 		},
 	},
-	width: {
-		options: ['vw', 'vh', '%', 'px', 'em', 'rem'],
-		scale: [1, 1],
-	},
-	height: {
-		options: ['vw', 'vh', '%', 'px', 'em', 'rem'],
-		scale: [1, 1],
-	},
-	padding: {
-		options: ['vw', 'vh', '%', 'px', 'em', 'rem'],
-		scale: [0.2, 0.5],
-	},
-	border: {
-		options: ['vw', 'vh', '%', 'px', 'em', 'rem'],
-		scale: [0.1, 0.25],
-	},
-	margin: {
-		options: ['vw', 'vh', '%', 'px', 'em', 'rem'],
-		scale: [0.2, 0.5],
+	boxModel: {
+		options: ['width', 'height', 'padding', 'border', 'margin'],
+		width: {
+			options: ['numeric', 'string'],
+			string: ['auto', 'max-content', 'min-content'],
+			numeric: {
+				options: ['px', 'rem', 'em', '%', 'vw', 'vh'],
+				px: {
+					range: [0, 1600],
+					step: 1,
+				},
+				rem: {
+					range: [0, 80],
+					step: 0.25,
+				},
+				em: {
+					range: [0, 80],
+					step: 0.25,
+				},
+				'%': {
+					range: [0, 100],
+					step: 1,
+				},
+				vw: {
+					range: [0, 100],
+					step: 1,
+				},
+				vh: {
+					range: [0, 100],
+					step: 1,
+				},
+			},
+		},
+		height: {
+			options: ['numeric', 'string'],
+			string: ['auto', 'max-content', 'min-content'],
+			numeric: {
+				options: ['px', 'rem', 'em', '%', 'vw', 'vh'],
+				px: {
+					range: [0, 1600],
+					step: 1,
+				},
+				rem: {
+					range: [0, 80],
+					step: 0.25,
+				},
+				em: {
+					range: [0, 80],
+					step: 0.25,
+				},
+				'%': {
+					range: [0, 100],
+					step: 1,
+				},
+				vw: {
+					range: [0, 100],
+					step: 1,
+				},
+				vh: {
+					range: [0, 100],
+					step: 1,
+				},
+			},
+		},
+		padding: {
+			options: ['numeric', 'string'],
+			string: ['auto', 'max-content', 'min-content'],
+			numeric: {
+				options: ['px', 'rem', 'em', '%', 'vw', 'vh'],
+				px: {
+					range: [0, 100],
+					step: 1,
+				},
+				rem: {
+					range: [0, 8],
+					step: 0.0625,
+				},
+				em: {
+					range: [0, 8],
+					step: 0.0625,
+				},
+				'%': {
+					range: [0, 20],
+					step: 0.2,
+				},
+				vw: {
+					range: [0, 20],
+					step: 0.2,
+				},
+				vh: {
+					range: [0, 20],
+					step: 0.2,
+				},
+			},
+		},
+		border: {
+			options: ['numeric', 'string'],
+			string: ['auto', 'max-content', 'min-content'],
+			numeric: {
+				options: ['px', 'rem', 'em', '%', 'vw', 'vh'],
+				px: {
+					range: [0, 25],
+					step: 0.25,
+				},
+				rem: {
+					range: [0, 8],
+					step: 0.0625,
+				},
+				em: {
+					range: [0, 4],
+					step: 0.0625,
+				},
+				'%': {
+					range: [0, 10],
+					step: 0.1,
+				},
+				vw: {
+					range: [0, 5],
+					step: 0.05,
+				},
+				vh: {
+					range: [0, 5],
+					step: 0.05,
+				},
+			},
+		},
+		margin: {
+			options: ['numeric', 'string'],
+			string: ['auto', 'max-content', 'min-content'],
+			numeric: {
+				options: ['px', 'rem', 'em', '%', 'vw', 'vh'],
+				px: {
+					range: [0, 100],
+					step: 1,
+				},
+				rem: {
+					range: [0, 8],
+					step: 0.0625,
+				},
+				em: {
+					range: [0, 8],
+					step: 0.0625,
+				},
+				'%': {
+					range: [0, 20],
+					step: 0.2,
+				},
+				vw: {
+					range: [0, 20],
+					step: 0.2,
+				},
+				vh: {
+					range: [0, 20],
+					step: 0.2,
+				},
+			},
+		},
 	},
 };
 
