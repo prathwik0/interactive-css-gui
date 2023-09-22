8d05811044b2b492ed3b95ee6840b6c4cae95439
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-22 19:53:41 +0530

    style: update CSSProperty.svelte

--- src/routes/CSSProperty.svelte
+++ src/routes/CSSProperty.svelte
@@ -4,7 +4,7 @@
 	export let selected: string = options[0];
 </script>
 
-<select bind:value={selected} class="inline-block max p-2 text-xs sm:text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+<select bind:value={selected} class="w-full p-2 text-xs sm:text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 	{#each options as option}
 		<option value={option}>
 			{title + ': ' + option}
